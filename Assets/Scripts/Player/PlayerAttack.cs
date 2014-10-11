3d24ce077bbc102a87af152fe70dcc95020af8f7
Ariequ <Ariequ>
2014-10-11 17:55:11 +0800

    new attack motion

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,93 +3,109 @@ using System.Collections;
 
 public class PlayerAttack : MonoBehaviour
 {
-	public float timeBetweenAttacks = 2f;
-	public int attackDamage = 10;
+    public float timeBetweenAttacks = 2f;
+    public int attackDamage = 10;
     
-	// 使用字符串变量保存当前状态，避免多处引用写错
-	private static readonly string IdleState = "Idle";
-	private static readonly string MoveState = "Move";
-	private static readonly string AtkSliceState = "attack01";
-	private static readonly string AtkStabState = "attack02";
-	private static readonly string AtkCleave = "attack03";
-	// 动画状态机参数Key
-	private static readonly string ActionCMD = "ActionCMD";
+    // 使用字符串变量保存当前状态，避免多处引用写错
+    private static readonly string IdleState = "Idle";
+    private static readonly string MoveState = "Locomotion";
+    private static readonly string ATTACK01 = "attack01";
+    private static readonly string ATTACK02 = "attack02";
+    private static readonly string ATTACK03 = "attack03";
+    private static readonly string ATTACK04 = "attack04";
+    private static readonly string ATTACK05 = "attack05";
 
-	[HideInInspector]
-	public Animator animator = null;
-	// 当前连击数（即 玩家按下攻击键的次数）
-	public int curComboCount = 0;
+    // 动画状态机参数Key
+    private static readonly string ActionCMD = "ActionCMD";
+    [HideInInspector]
+    public Animator
+        animator = null;
+    // 当前连击数（即 玩家按下攻击键的次数）
+    public int curComboCount = 0;
+    [HideInInspector]
+    public bool
+        canMakeDamage;
 
-	[HideInInspector]
-	public bool canMakeDamage;
+    void Awake()
+    {
+        animator = GetComponent <Animator>();
+    }
 
-	void Awake()
-	{
-		animator = GetComponent <Animator>();
-	}
-
-	void FixedUpdate()
-	{
-		AnimatorStateInfo stateInfo = this.animator.GetCurrentAnimatorStateInfo(0);
-		if (!stateInfo.IsName(IdleState))
-		{
-			// 每次设置完参数之后，都应该在下一帧开始时将参数设置清空，避免连续切换
-			this.animator.SetInteger(ActionCMD, 0);
-		}
+    void FixedUpdate()
+    {
+        AnimatorStateInfo stateInfo = this.animator.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsName(IdleState))
+        {
+            // 每次设置完参数之后，都应该在下一帧开始时将参数设置清空，避免连续切换
+            this.animator.SetInteger(ActionCMD, 0);
+        }
         
-		if (stateInfo.IsName(AtkSliceState) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 2))
-		{
-			// 当在攻击1状态下，并且当前状态运行了0.6正交化时间（即动作时长的60%），并且用户在攻击1状态下又按下了“攻击键”
-			this.animator.SetInteger(ActionCMD, 1);
-			canMakeDamage = true;
-		}
-		else if (stateInfo.IsName(AtkStabState) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 3))
-		{
-			// 挡在攻击2状态下（同理攻击1状态）
-			this.animator.SetInteger(ActionCMD, 1);
-			canMakeDamage = true;
-		}  
+        if (stateInfo.IsName(ATTACK01) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 2))
+        {
+            // 当在攻击1状态下，并且当前状态运行了0.6正交化时间（即动作时长的60%），并且用户在攻击1状态下又按下了“攻击键”
+            this.animator.SetInteger(ActionCMD, 1);
+        }
+        else if (stateInfo.IsName(ATTACK02) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 3))
+        {
+            // 挡在攻击2状态下（同理攻击1状态）
+            this.animator.SetInteger(ActionCMD, 1);
+        }
+        else if (stateInfo.IsName(ATTACK03) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 4))
+        {
+            // 挡在攻击3状态下（同理攻击1状态）
+            this.animator.SetInteger(ActionCMD, 1);
+        } 
+        else if (stateInfo.IsName(ATTACK04) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 5))
+        {
+            // 挡在攻击3状态下（同理攻击1状态）
+            this.animator.SetInteger(ActionCMD, 1);
+        } 
+        else if (stateInfo.IsName(ATTACK05) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 6))
+        {
+            // 挡在攻击3状态下（同理攻击1状态）
+            this.animator.SetInteger(ActionCMD, 1);
+        } 
 
-		else if (stateInfo.IsName(AtkCleave) && (stateInfo.normalizedTime > 0.8f) && (this.curComboCount == 4))
-		{
-			// 挡在攻击3状态下（同理攻击1状态）
-			this.animator.SetInteger(ActionCMD, 1);
-			canMakeDamage = true;
-		}  
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            // 监听用户输入（假设J键为攻击键）
+            Attack();
+        }
+    }
 
-		if (Input.GetKeyUp(KeyCode.J))
-		{
-			// 监听用户输入（假设J键为攻击键）
-			Attack();
-		}
-	}
-
-	void Attack()
-	{
-		AnimatorStateInfo stateInfo = this.animator.GetCurrentAnimatorStateInfo(0);
-		if (stateInfo.IsName(IdleState) || stateInfo.IsName(MoveState))
-		{
-			// 在待命状态下，按下攻击键，进入攻击1状态，并记录连击数为1
-			this.animator.SetInteger(ActionCMD, 1);
-			this.curComboCount = 1;
-		}
-		else
-		if (stateInfo.IsName(AtkSliceState))
-		{
-			// 在攻击1状态下，按下攻击键，记录连击数为2（切换状态在Update()中）
-			this.curComboCount = 2;
-		}
-		else
-		if (stateInfo.IsName(AtkStabState))
-		{
-			// 在攻击2状态下，按下攻击键，记录连击数为3（切换状态在Update()中）
-			this.curComboCount = 3;
-		}
-		else
-		if (stateInfo.IsName(AtkCleave))
-		{
-			// 在攻击3状态下，按下攻击键，记录连击数为4（切换状态在Update()中）
-			this.curComboCount = 4;
-		}
-	}
+    void Attack()
+    {
+        AnimatorStateInfo stateInfo = this.animator.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName(IdleState) || stateInfo.IsName(MoveState))
+        {
+            // 在待命状态下，按下攻击键，进入攻击1状态，并记录连击数为1
+            this.animator.SetInteger(ActionCMD, 1);
+            this.curComboCount = 1;
+        }
+        else if (stateInfo.IsName(ATTACK01))
+        {
+            // 在攻击1状态下，按下攻击键，记录连击数为2（切换状态在Update()中）
+            this.curComboCount = 2;
+        }
+        else if (stateInfo.IsName(ATTACK02))
+        {
+            // 在攻击2状态下，按下攻击键，记录连击数为3（切换状态在Update()中）
+            this.curComboCount = 3;
+        }
+        else if (stateInfo.IsName(ATTACK03))
+        {
+            // 在攻击3状态下，按下攻击键，记录连击数为4（切换状态在Update()中）
+            this.curComboCount = 4;
+        }
+        else if (stateInfo.IsName(ATTACK04))
+        {
+            // 在攻击3状态下，按下攻击键，记录连击数为4（切换状态在Update()中）
+            this.curComboCount = 5;
+        }
+        else if (stateInfo.IsName(ATTACK05))
+        {
+            // 在攻击3状态下，按下攻击键，记录连击数为4（切换状态在Update()中）
+            this.curComboCount = 6;
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,16 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Animator animator;
     private float speed = 0;
-    private Vector3 direction;
-    private Vector3 movement;                   // The vector to store the direction of the player's movement.
-    private Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    private Vector3 direction;         
     private int m_SpeedId = 0;
     
     // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
-        playerRigidbody = GetComponent <Rigidbody> ();
         m_SpeedId = Animator.StringToHash("Speed");     
     }
     
@@ -24,35 +21,20 @@ public class PlayerMovement : MonoBehaviour
     {
         AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
 
-        if (animator && Camera.main)
-        {   
-            JoystickToEvents.Do(transform, Camera.main.transform, ref speed, ref direction);           
-            animator.SetFloat(m_SpeedId, speed);//, 0.1f, Time.deltaTime);   
-        }   
-      
-        if (state.IsName("Idle") || state.IsName("Move") && !state.IsTag("Attack"))
+        if (state.IsName("Idle") || state.IsName("Locomotion"))
         {
-			transform.rotation = Quaternion.Lerp(transform.rotation, MathUtils.LookRotationXZ(direction), Time.deltaTime * 10); 
-			Move(direction.x, direction.z);
+            JoystickToEvents.Do(transform, Camera.main.transform, ref speed, ref direction);           
+            animator.SetFloat(m_SpeedId, speed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, MathUtils.LookRotationXZ(direction), Time.deltaTime * 10); 
+        }
+        else
+        {
+            animator.SetFloat(m_SpeedId, 0);
         }
 
 		if (state.IsName("Idle"))
 		{
 			animator.SetBool("Attacked", false);
-			animator.SetInteger("HurtType", 0);
 		}
-    }
-
-    void Move (float h, float v)
-    {
-        // Set the movement vector based on the axis input.
-        movement.Set (h, 0f, v);
-        
-        // Normalise the movement vector and make it proportional to the speed per second.
-        movement = movement.normalized * speed * Time.deltaTime * 6;
-        
-        // Move the player to it's current position plus the movement.
-//        playerRigidbody.MovePosition (transform.position + movement);
-		transform.Translate(movement,Space.World);
     }
 }
